2a2a1f96eb63dfd59c26293cc14142b4f04c5450
Nandni vashistha <nandnivashistha@gmail.com>
2024-06-04 12:29:46 +0530

    Update sanity.ts

--- app/lib/sanity.ts
+++ app/lib/sanity.ts
@@ -4,7 +4,7 @@ import imageUrlBuilder from "@sanity/image-url";
 export const client = createClient({
   apiVersion: "2023-05-03",
   dataset: "production",
-  projectId: "m9mkh2nr",
+  projectId: "at8sh4wj",
   useCdn: false,
 });
 
